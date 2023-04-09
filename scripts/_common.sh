cfe621ed4c3765824a67df44199489f6d5ba37ee
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-04-09 23:38:58 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,13 +6,6 @@
 # PHP APP SPECIFIC
 #=================================================
 
-#REMOVEME? YNH_PHP_VERSION="8.0"
-
-#REMOVEME? php_dependencies="php${YNH_PHP_VERSION} php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl  php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-gnupg"
-
-# dependencies used by the app (must be on a single line)
-#REMOVEME? pkg_dependencies="$php_dependencies"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
