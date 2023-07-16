dfe5bf758f6e0c299e80b2ee9d67580915ddf274
Robles Rodolphe <rodolphe.robles@sfr.fr>
2023-07-16 23:58:02 +0200

    Update _common.sh
    need php-mbstring dependencies

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,7 +8,7 @@
 
 YNH_PHP_VERSION="8.0"
 
-php_dependencies="php${YNH_PHP_VERSION} php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl  php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-gnupg"
+php_dependencies="php${YNH_PHP_VERSION} php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl  php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-gnupg php${YNH_PHP_VERSION}-mbstring"
 
 # dependencies used by the app (must be on a single line)
 pkg_dependencies="$php_dependencies"
