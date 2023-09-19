dc90c24f609f79c70404c335636ea8d937b5d8a4
rodinux <rodolphe.robles@sfr.fr>
2023-09-19 09:14:43 +0200

    ready to merge

--- conf/config.local.php
+++ conf/config.local.php
@@ -3,7 +3,7 @@
 /**
  * NE PAS MODIFIER CE FICHIER. Il est regénéré automatiquement à chaque mise à jour de Yunohost
  *
- * Pour ajouter vos configurations personnalisées, rendez-vous dans le fichier config.local.user.php
+ * Pour ajouter vos configurations personnalisées, rendez-vous dans le fichier data/config.local.user.php
  */
 require '__INSTALL_DIR__/config.local.yunohost.php';
 require '__INSTALL_DIR__/data/config.local.user.php';
