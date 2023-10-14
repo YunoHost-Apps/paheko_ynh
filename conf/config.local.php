6ae01ab4d51fe982a9a235473cf178ab8229383f
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-10-14 23:10:12 +0200

    cleaning

--- conf/config.local.php
+++ conf/config.local.php
@@ -634,7 +634,7 @@ const MAIL_SENDER = 'associations@__DOMAIN__';
  * Défaut : null (dans ce cas c'est le stockage qui détermine la taille disponible, donc généralement l'espace dispo sur le disque dur !)
  */
 
-//const FILE_STORAGE_QUOTA = 10*1024*1024; // Forcer le quota alloué à 10 Mo, quel que soit le backend de stockage
+const FILE_STORAGE_QUOTA = 10*1024*1024; // Forcer le quota alloué à 10 Mo, quel que soit le backend de stockage
 
 /**
  * FILE_VERSIONING_POLICY
