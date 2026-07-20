013ef59c8e4f839e398cb4fc36916a36dce7e385
Thatoo <Thatoo@users.noreply.github.com>
2026-07-20 12:29:40 +0200

    Update config.local.user.php

--- conf/config.local.user.php
+++ conf/config.local.user.php
@@ -8,21 +8,47 @@
  * Pour une nouvelle installation, ce fichier sera pris en compte après la première mise à jour.
  */
 
-// Nécessaire pour situer les constantes dans le bon namespace
-namespace Paheko;
+<?php
+
+/**
+ * Ce fichier est ici pour ajouter des personnalisations à votre configuration.
+ *
+ * Modifiez ce fichier à votre guise, il ne sera pas modifié lors des prochaines mises à jour.
+ * Consultez le fichier __INSTALL_DIR__/config.local.php pour choisir des constantes à ajouter.
+ * Pour une nouvelle installation, ce fichier sera pris en compte après la première mise à jour.
+ */
 
 if (array_key_exists('REMOTE_USER', $_SERVER)) {
 	$username = $_SERVER["REMOTE_USER"];
-	define('Paheko\LOCAL_LOGIN', [
-		'user' => [
-			'_name' => $username,
-		],
-		'permissions' => [
-			'users'      => 9,
-			'accounting' => 9,
-			'web'        => 9,
-			'documents'  => 9,
-			'config'     => 9,
-		],
-	]
-);}
+	$db = new SQLite3('/home/yunohost.app/paheko/data/association.sqlite');
+
+	$stmt = $db->prepare('SELECT id FROM users WHERE identifiant_adhesion = :username');
+	$stmt->bindValue(':username', $username, SQLITE3_TEXT);
+
+	$result = $stmt->execute();
+	$row = $result->fetchArray(SQLITE3_ASSOC);
+
+	$user_id = $db->querySingle('SELECT id FROM users WHERE identifiant_adhesion="'.$username.'"');
+	if ($user_id) {
+		define('Paheko\LOCAL_LOGIN', $user_id);
+	}
+	else {
+		$permission = 0;
+// ajouter les noms d'utilisateurices de yunohost qui n'ont pas de compte paheko dans l'array avec lesquels vous souhaitez administrer paheko sans créer de compte
+		if (in_array($username, [""])) {
+			$permission = 9;
+		}
+		define('Paheko\LOCAL_LOGIN', [
+			'user' => [
+				'_name' => $username,
+			],
+			'permissions' => [
+				'users'      => $permission,
+				'accounting' => $permission,
+				'web'        => $permission,
+				'documents'  => $permission,
+				'config'     => $permission,
+			],
+		]);
+	}
+}
